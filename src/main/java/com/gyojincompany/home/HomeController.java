2910d6fa38bdbb289ce68d037a7e8193d17e09a6
gyojincompany <luckyred@gmail.com>
2022-12-07 15:45:41 +0900

    th:href 추가

--- src/main/java/com/gyojincompany/home/HomeController.java
+++ src/main/java/com/gyojincompany/home/HomeController.java
@@ -3,6 +3,8 @@ package com.gyojincompany.home;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -74,4 +76,54 @@ public class HomeController {
 		return "member/ifTest";
 	}
 
+	
+	@RequestMapping(value = "elseif")
+	public String elseif(Model model) {
+		
+		List<MemberDto> memberDtos = new ArrayList<MemberDto>();
+		
+		MemberDto memberDto1 = new MemberDto();
+		memberDto1.setAge(10);
+		memberDto1.setName("이순신");
+		
+		MemberDto memberDto2 = new MemberDto();
+		memberDto2.setAge(20);
+		memberDto2.setName("김유신");
+		
+		MemberDto memberDto3 = new MemberDto();
+		memberDto3.setAge(30);
+		memberDto3.setName("강감찬");
+		
+//		memberDtos.add(memberDto1);
+//		memberDtos.add(memberDto2);
+//		memberDtos.add(memberDto3);
+		
+		model.addAttribute("member1", memberDto1);
+		model.addAttribute("member2", memberDto2);
+		model.addAttribute("member3", memberDto3);
+		
+		
+		return "member/elseifTest";
+	}
+	
+	@RequestMapping(value = "/hreftest")
+	public String hrefTest(Model model) {
+		
+		
+		
+		return "member/hrefTest";
+	}
+	
+	@RequestMapping(value = "login")
+	public String login(HttpServletRequest request, Model model) {
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		model.addAttribute("id", id);
+		model.addAttribute("pw", pw);
+		
+		return "member/login";
+	}
+	
 }
